f35a7b3b9e9581118bfa1d2db7313785e4d5066d
afeibuhuifei <imchuzi@gmail.com>
2025-10-21 13:21:18 +0800

    feat(ui): add footer with author attribution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Page() {
 	return (
 		<main className="h-screen w-screen">
 			<Scene />
+			<footer className="fixed bottom-4 left-1/2 transform -translate-x-1/2 text-white/70 text-sm pointer-events-none z-10">
+				[by afei @2025]
+			</footer>
 		</main>
 	)
 }
